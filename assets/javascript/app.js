0e383a075a61b48c3dcb193a278fca13c0b3b5c0
Priya Iyar <Priya Iyar>
2017-03-22 21:47:47 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ renderButtons();
 function searchGif(animal) {
 
 
-    var url = "https://api.giphy.com/v1/gifs/search?q=" +
+    var url = "http://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
